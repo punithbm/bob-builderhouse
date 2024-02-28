f93f502e3df2e721e8b4b4c236eda04b4617be89
Murugan D <yasmuru@users.noreply.github.com>
2024-02-28 13:27:18 +0530

    Add APIs to interact with contract

---
+++ backend/app/service.py
@@ -0,0 +1,67 @@
+import json
+
+from django.conf import settings
+
+from web3 import Web3, Account
+from web3.middleware import geth_poa_middleware
+
+class ChainService:
+    def __init__(self):
+        self.web3 = Web3(Web3.HTTPProvider(settings.BLOCKCHAIN_RPC_URL))
+        self.web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+        self.contract_address = Web3.to_checksum_address(settings.BLOCKCHAIN_CONTRACT_ADDRESS)
+        self.account = Account.from_key(settings.ACCOUNT_PRIVATE_KEY)
+        with open('../contracts/hello_bitcoin_contract_abi.json', 'r') as abi_definition:
+            self.contract_abi = json.load(abi_definition)
+        self.contract = self.web3.eth.contract(address=self.contract_address, abi=self.contract_abi)
+    def is_connected(self):
+        return self.web3.isConnected()
+    
+    
+    def send_transaction(self, function_name, *args):
+        nonce = self.web3.eth.get_transaction_count(self.account.address)
+        contract_function = self.contract.functions[function_name](*args)
+        transaction = contract_function.build_transaction({
+            'from': self.account.address,
+            'chainId': int(settings.BLOCKCHAIN_ID),
+            'gas': 2000000,
+            'gasPrice': self.web3.to_wei('50', 'gwei'),
+            'nonce': nonce,
+        })
+        signed_txn = self.account.sign_transaction(transaction)
+        txn_hash = self.web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+        return self.web3.to_hex(txn_hash)
+
+    def get_tx_receipt(self, tx_hash):
+        try:
+            # print(self.web3.eth)
+            # Fetch the transaction receipt using its hash
+            tx_receipt = self.web3.eth.wait_for_transaction_receipt(tx_hash)
+            return tx_receipt, None
+        except Exception as e:
+            print(f"Error fetching transaction receipt: {e}")
+            return None, e
+    
+    def get_native_token_balance(self, address):
+        balance_wei = self.web3.eth.get_balance(address)
+        balance_ether = self.web3.from_wei(balance_wei, 'ether')
+        return balance_ether
+    
+    def get_token_balance(self, address, token_address):
+        token_abi = '''
+        [
+            {
+                "constant":true,
+                "inputs":[{"name":"_owner","type":"address"}],
+                "name":"balanceOf",
+                "outputs":[{"name":"balance","type":"uint256"}],
+                "type":"function"
+            }
+        ]
+        '''
+        # Setup the contract
+        token_contract = self.web3.eth.contract(address=token_address, abi=token_abi)
+
+        # Call the balanceOf function
+        balance = token_contract.functions.balanceOf(address).call()
+        return balancesites 
---
+++ backend/app/views.py
@@ -0,0 +1,72 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from decouple import config
+
+from app import decorator, models, serializers
+from bob import constants as const
+
+from .service import ChainService
+import requests
+
+
+@api_view(['POST'])
+@decorator.valid_json
+def AcceptBtcSellOrder(request):
+    data = request.input_data
+    service = ChainService()
+    btc_address = (data.get("btc_address").encode('utf-8'))
+    tx_hash = service.send_transaction(
+        'acceptBtcSellOrder',
+        data.get('order_id'),
+        (btc_address, ),
+    )
+    response = {"tx_hash": tx_hash}
+    # receipt, err = service.get_tx_receipt(tx_hash)
+    # response['status'] = "success" if receipt and receipt['status'] else "failed"
+    return JsonResponse(response)
+
+@api_view(['POST'])
+@decorator.valid_json
+def GetTransactionStatus(request):
+    data = request.input_data
+    service = ChainService()
+    response = {"tx_hash": data.get('tx_hash')}
+    receipt, err = service.get_tx_receipt(data.get('tx_hash'))
+    response['status'] = "success" if receipt and receipt['status'] else "failed"
+    return JsonResponse(response)
+
+
+@api_view(['GET'])
+def GetBitCoinBalance(request, address):
+    url = config("UNISAT_API") + f"address/{address}/balance"
+
+    payload = {}
+    headers = {
+    'Authorization': 'Bearer ' + config('UNISAT_API_KEY')
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return JsonResponse(response.json())
+
+@api_view(['GET'])
+def GetBobTokensBalance(request, address):
+    response = {}
+    service = ChainService()
+    response['balance'] = service.get_native_token_balance(address)
+    tokens = []
+    usdc_token = {
+        'address': '0x27c3321E40f039d10D5FF831F528C9CEAE601B1d',
+        'symbol': 'USDC',
+        'decimal': 18,
+        'balance': service.get_token_balance(address, '0x27c3321E40f039d10D5FF831F528C9CEAE601B1d')
+    }
+    tokens.append(usdc_token)
+    wbtc_token = {
+        'address': '0x2868d708e442A6a940670d26100036d426F1e16b',
+        'symbol': 'WBTC',
+        'decimal': 8,
+        'balance': service.get_token_balance(address, '0x2868d708e442A6a940670d26100036d426F1e16b')
+    }
+    tokens.append(wbtc_token)
+    response['tokens'] = tokens
+    return JsonResponse(response)
